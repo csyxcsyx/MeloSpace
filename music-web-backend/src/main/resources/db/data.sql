0fb17066afbe5be27aa7160f4604ea83e4930ac5
Rex Fang <1977817316@qq.com>
2026-06-30 02:07:04 +0800

    Revert "feat: add remaining 11 songs to seed data (爱琴海, 那天下雨了, 女儿殿下, 七月的极光, 谁稀罕, 圣诞星, 圣徒, 淘金小镇, 西西里, 乡间的路, 湘女多情)"
    This reverts commit 1b6617be7f0f831d0d6d392bc5cdfb2e7977cddb.

--- music-web-backend/src/main/resources/db/data.sql
+++ music-web-backend/src/main/resources/db/data.sql
@@ -27,18 +27,7 @@ ON DUPLICATE KEY UPDATE
 INSERT INTO song (id, title, artist_id, album_id, cover_url, audio_url, lyric_url, duration_seconds, language, genre, mood, status)
 VALUES
   (1, 'I Do', 1, 1, NULL, '/media/audio/I%20Do%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '浪漫', 1),
-  (2, '太阳之子', 1, 1, NULL, '/media/audio/%E5%A4%AA%E9%98%B3%E4%B9%8B%E5%AD%90%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '热烈', 1),
-  (3, '爱琴海', 1, 1, NULL, '/media/audio/%E7%88%B1%E7%90%B4%E6%B5%B7%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '悠扬', 1),
-  (4, '那天下雨了', 1, 1, NULL, '/media/audio/%E9%82%A3%E5%A4%A9%E4%B8%8B%E9%9B%A8%E4%BA%86%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '伤感', 1),
-  (5, '女儿殿下', 1, 1, NULL, '/media/audio/%E5%A5%B3%E5%84%BF%E6%AE%BF%E4%B8%8B%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '典雅', 1),
-  (6, '七月的极光', 1, 1, NULL, '/media/audio/%E4%B8%83%E6%9C%88%E7%9A%84%E6%9E%81%E5%85%89%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '梦幻', 1),
-  (7, '谁稀罕', 1, 1, NULL, '/media/audio/%E8%B0%81%E7%A8%80%E7%BD%95%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '洒脱', 1),
-  (8, '圣诞星', 1, 1, NULL, '/media/audio/%E5%9C%A3%E8%AF%9E%E6%98%9F%20(feat.%20%E6%9D%A8%E7%91%9E%E4%BB%A3)%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '温暖', 1),
-  (9, '圣徒', 1, 1, NULL, '/media/audio/%E5%9C%A3%E5%BE%92%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '神圣', 1),
-  (10, '淘金小镇', 1, 1, NULL, '/media/audio/%E6%B7%98%E9%87%91%E5%B0%8F%E9%95%87%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '欢快', 1),
-  (11, '西西里', 1, 1, NULL, '/media/audio/%E8%A5%BF%E8%A5%BF%E9%87%8C%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '异域', 1),
-  (12, '乡间的路', 1, 1, NULL, '/media/audio/%E4%B9%A1%E9%97%B4%E7%9A%84%E8%B7%AF%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '清新', 1),
-  (13, '湘女多情', 1, 1, NULL, '/media/audio/%E6%B9%98%E5%A5%B3%E5%A4%9A%E6%83%85%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '温柔', 1)
+  (2, '太阳之子', 1, 1, NULL, '/media/audio/%E5%A4%AA%E9%98%B3%E4%B9%8B%E5%AD%90%20-%20%E5%91%A8%E6%9D%B0%E4%BC%A6.flac', NULL, 0, '中文', 'Pop', '热烈', 1)
 ON DUPLICATE KEY UPDATE
   audio_url = VALUES(audio_url),
   status = VALUES(status);
