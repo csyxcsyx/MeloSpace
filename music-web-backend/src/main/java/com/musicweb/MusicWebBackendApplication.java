620285c88ebf175ddd55ace3cdc62beee9b3b4e5
Rex Fang <1977817316@qq.com>
2026-06-29 23:16:18 +0800

    chore: scaffold backend service

---
+++ music-web-backend/src/main/java/com/musicweb/MusicWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWebBackendApplication.class, args);
+	}
+
+}
